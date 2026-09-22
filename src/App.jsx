23f57d8fd1c52478c9ade982c612c0130d5a1236
Pratham <prathamshiroya7@gmail.com>
2026-09-22 22:59:52 +0530

    Add @vercel/analytics dependency and integrate Analytics component into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Menu from "./components/Menu";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
       <Menu />
+      <Analytics/>
     </>
   );
 }
